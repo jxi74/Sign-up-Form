c50331c33fb8e67bad761c0f9d48005ea3f1667b
jxi74 <112279011+jxi74@users.noreply.github.com>
2024-07-12 22:40:23 -0400

    Add finished form and formatting

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,3 +1,20 @@
-let password = document.querySelector("#password");
-let password_confirm = document.querySelector("#password_confirm");
+const password = document.querySelector("#password_init");
+const password_confirm = document.querySelector("#password_confirm");
 
+// call checkPasswords after every keyboard input on confirm password field
+password.addEventListener("input", checkPasswords);
+password_confirm.addEventListener("input", checkPasswords);
+
+function checkPasswords() {
+    // if values are not equal, mark inputs as :invalid
+    // and report to update form
+    if (password.value !== password_confirm.value) {
+        password.setCustomValidity("Passwords must match");
+        password_confirm.setCustomValidity("Passwords must match");
+    }
+    else {
+        // mark as valid and report to update form
+        password.setCustomValidity("");
+        password_confirm.setCustomValidity("");
+    }
+}
